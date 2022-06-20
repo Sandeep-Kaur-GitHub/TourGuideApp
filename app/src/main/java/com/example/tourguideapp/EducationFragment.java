196cd2dfba216da3b3d4359e77661a2eca87d9c7
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-06-20 12:22:09 +0530

    Add Arraylist and custom java classes

--- app/src/main/java/com/example/tourguideapp/EducationFragment.java
+++ app/src/main/java/com/example/tourguideapp/EducationFragment.java
@@ -1,64 +1,37 @@
 package com.example.tourguideapp;
 
+import android.content.Context;
+import android.nfc.Tag;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EducationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class EducationFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EducationFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EducationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EducationFragment newInstance(String param1, String param2) {
-        EducationFragment fragment = new EducationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
+ Context mContext;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_education, container, false);
+
+        ArrayList<Word> array_list = new ArrayList<Word>();
+        array_list.add(new Word("PTU University","hhh",R.drawable.ptu));
+        array_list.add(new Word("PTU University","jhg",R.drawable.ptu));
+      //  array_list.add(new Word("PTU University",mContext.getString(R.string.ptudescription),R.drawable.ptu));
+
+
+        Log.i("", "hey" + array_list);
+        View view = inflater.inflate(R.layout.fragment_education, container, false);
+        ListView list_view = (ListView) view.findViewById(R.id.list_view);
+        WordAdapter word_adapter = new WordAdapter(this, array_list);
+        list_view.setAdapter(word_adapter);
+        return view;
     }
+
 }
